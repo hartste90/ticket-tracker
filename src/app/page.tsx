815cd00f9e99c9d19b953144b05b3fe47e5c14f6
Steve Hart <hartste90@gmail.com>
2024-07-24 23:39:22 -0700

    added firebase initialization to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,28 @@ import Image from "next/image";
 import React from "react";
 import { useState } from "react";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCOx41pT-4pciiPz7owccGOa1d0lp4ObjM",
+  authDomain: "ticket-tracker-61a63.firebaseapp.com",
+  projectId: "ticket-tracker-61a63",
+  storageBucket: "ticket-tracker-61a63.appspot.com",
+  messagingSenderId: "893361691754",
+  appId: "1:893361691754:web:ceaa5a66af984fbfeffe13",
+  measurementId: "G-1JP8DVVDEH",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 export default function Home() {
   const [status, setStatus] = useState("initializing");
 
